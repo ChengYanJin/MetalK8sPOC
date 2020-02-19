6bde55b660a74cbcf22bfe56a1f10acb11f7116e
YanJin <yanjin.cheng@scality.com>
2020-02-19 18:06:40 +0100

    apply webpack-external-inport
    - create seperate chunk with hash for the components
    - use rimraf to clean the build folder before the current build

---
+++ src/Toto.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Toto = () => {
+  return <div>I am Toto</div>;
+};
+
+export default Toto;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Hello from "./Hello";
+import Toto from "./Toto";
 ReactDOM.render(
-  <div>hello, I am metalK8s</div>,
+  <div>
+    hello, I am metalK8s
+    <Hello />
+    <Toto />
+  </div>,
   document.getElementById("root")
 );
